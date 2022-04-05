60f57be61a2b76d8a17f63e975211528f764ae3e
GilsonRavaiani <gravaiani@yahoo.com.br>
2022-04-05 07:58:04 -0300

    StarsWith_EndsWith

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StartsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+
+            //Texto começa com ("")
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este"));
+            Console.WriteLine(texto.StartsWith("texto"));
+            Console.WriteLine("");
+
+            //Texto termina com ("")
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("Teste"));
+            Console.WriteLine(texto.EndsWith("xpro"));
+        }
+    }
+}
